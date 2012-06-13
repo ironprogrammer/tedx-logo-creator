47a59175ebb6f6d7943f327448d03a709b259a99
Brian Alexander <brian@brianalexander.com>
2012-06-13 14:32:17 -0700

    added kerning where needed, spacing for decenders

--- image.php
+++ image.php
@@ -7,19 +7,23 @@ header("Pragma: no-cache");
 header("Content-type: application/force-download");
 
 switch ($_GET["type"]) {
-	case '1':	tedx("w", 240, 420, 136, 32, 162, 395, 162);	break;
-	case '2':	tedx("w", 290, 420, 136, 32, 162, 32, 214);	break;
-	case '3':	tedx("w", 370, 30, 267, 32, 293, 395, 293);	break;
-	case '4':	tedx("w", 420, 30, 267, 32, 293, 32, 345);	break;
+	case '1':	tedx("w", 240, 420, 136, 32, 170, 395, 170);	break;
+	case '2':	tedx("w", 290, 420, 136, 32, 170, 32, 214);	break;
+	case '3':	tedx("w", 370, 30, 267, 32, 300, 395, 300);	break;
+	case '4':	tedx("w", 420, 30, 267, 32, 295, 32, 345);	break;
 
-	case '5':	tedx("b", 240, 420, 136, 32, 162, 395, 162);	break;
-	case '6':	tedx("b", 290, 420, 136, 32, 162, 32, 214);	break;
-	case '7':	tedx("b", 370, 30, 267, 32, 293, 395, 293);	break;
-	default:	tedx("b", 420, 30, 267, 32, 293, 32, 345);	break;
+	case '5':	tedx("b", 240, 420, 136, 32, 170, 395, 170);	break;
+	case '6':	tedx("b", 290, 420, 136, 32, 170, 32, 214);	break;
+	case '7':	tedx("b", 370, 30, 267, 32, 300, 395, 300);	break;
+	default:	tedx("b", 420, 30, 267, 32, 295, 32, 345);	break;
 }
 
 function tedx($color, $height, $name_x, $name_y, $tag1_x, $tag1_y, $tag2_x, $tag2_y) {
-	$MAX_CHARS = 20;
+	$MAX_CHARS = 40;
+	$FONT_FILE = "Helvetica.ttf";
+	$TRACKING = 10;
+	$TRACKING_EXTRA = 5;
+	
 	$t = urldecode(substr($_GET["name"], 0, $MAX_CHARS));
 	if (substr($t, 0, 4) == "TEDx") $t = substr($t, 4);
 	$width = strlen($t)*110 + ($name_x == "420" ? 450 : 0);
@@ -33,15 +37,23 @@ function tedx($color, $height, $name_x, $name_y, $tag1_x, $tag1_y, $tag2_x, $tag
 	imagecopy($img, imagecreatefrompng("independently" . $color . ".png"), $tag1_x, $tag1_y, 0, 0, 354, 43);
 	imagecopy($img, imagecreatefrompng("organized" . $color . ".png"), $tag2_x, $tag2_y, 0, 0, 420, 42);
 
+	$pval = null;
 	for($i=0;$i<strlen($t);$i++) {
 		$value=substr($t,$i,1);
-		if($pval){
-			list($lx,$ly,$rx,$ry) = imagettfbbox(110,0,"Helvetica.ttf",$pval);
-			$nxpos+=$rx+3;
+		if( ! is_null( $pval ) ){
+			list($lx,$ly,$rx,$ry) = imagettfbbox(110,0,$FONT_FILE,$pval);
+			if ( preg_match('/[Aafjtvwxy]/', $pval) )
+				$nxpos += $rx;
+			elseif ( preg_match('/[u]/', $pval) )
+				$nxpos += $rx + $TRACKING_EXTRA + $TRACKING;
+			elseif ( preg_match('/[1]/', $pval) )
+				$nxpos += $rx + ($TRACKING_EXTRA * 2) + $TRACKING;
+			else
+				$nxpos += $rx + $TRACKING;
 		} else {
 			$nxpos=0;
 		}
-		imagettftext($img, 110, 0, $nxpos + $name_x, $name_y, ($color == "w" ? imagecolorallocate($img, 0, 0, 0) : imagecolorallocate($img, 255, 255, 255)), "Helvetica.ttf", $value);
+		imagettftext($img, 110, 0, $nxpos + $name_x, $name_y, ($color == "w" ? imagecolorallocate($img, 0, 0, 0) : imagecolorallocate($img, 255, 255, 255)), $FONT_FILE, $value);
 		$pval=$value;
 	}
 
